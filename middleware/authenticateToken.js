19514f8516f0276d278a0cc3e22cf0f232a5422a
timmySpark <timmyspark1@gmail.com>
2024-08-01 05:11:15 +0100

    [feat] :- created feature that authenticate user token

---
+++ middleware/authenticateToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware to authenticate requests
+
+const authenticateToken = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(401).json({ message: 'Invalid or expired token' });
+    }
+};
+
+module.exports = authenticateToken;
