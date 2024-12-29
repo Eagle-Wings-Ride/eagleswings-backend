9f42c4731e274226bb3548898096c055378195f7
timmySpark <timmyspark1@gmail.com>
2024-12-29 13:55:28 +0100

    [feat]:- added rates feature, connected to app js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express()
 const users = require('./routes/user')
 const booking = require('./routes/bookings')
+const rates = require('./routes/rates')
 const connectDB = require('./db/connect_db')
 require('dotenv').config()
 
@@ -13,6 +14,8 @@ app.use(express.json())
 // Routes
 app.use('/api/v1/users', users)
 app.use('/api/v1/book', booking)
+app.use('/api/v1/rates', rates)
+
 
 const port = process.env.PORT || 5000;
 
