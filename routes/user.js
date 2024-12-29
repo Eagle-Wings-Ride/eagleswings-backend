f0d1f267e479ce5c5a3925b6eece33e6fb404a39
timmySpark <timmyspark1@gmail.com>
2024-12-29 14:46:57 +0100

    [fix] :- renamed get child ny user route

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const authenticateToken = require('../middleware/authenticateToken')
 
 const {registerUser, loginUser, logoutUser, verifyUserOTP} = require('../controllers/userAuthCtrl')
 const {getAllUsers, getUser, currentUser, updateUser, deleteUser } = require('../controllers/userCtrl')
-const {addChild, getChild, getChildByUser, updateChild, deleteChild} = require('../controllers/childCtrl')
+const {addChild, getChild,getChildrenByUserId, updateChild, deleteChild} = require('../controllers/childCtrl')
 
 // User Route
 router.route('/').get(authenticateToken, getAllUsers)
@@ -22,7 +22,8 @@ router.route('/child/').post(authenticateToken, addChild)
 router.route('/child/:id').get(authenticateToken, getChild)
                         .patch(authenticateToken, updateChild)
                         .delete(authenticateToken, deleteChild)
-router.route('/my-children').get( authenticateToken, getChildByUser)
+router.route('/children/:userId')
+    .get(authenticateToken, getChildrenByUserId);
 
 
 module.exports = router
