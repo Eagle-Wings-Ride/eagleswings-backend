37eddcf909453aafe61f8b114319e71fe72a95ee
timmySpark <timmyspark1@gmail.com>
2025-01-19 10:41:24 +0100

    [feat] :- change message type for email ver to be more flexible

--- utils/sendVerificationEmail.js
+++ utils/sendVerificationEmail.js
@@ -1,17 +1,17 @@
 const sendEmailEthereal = require('../controllers/sendEmail')
 
-const sendVerificationEmail = async ({
-    name,
-    email,
-    otp
-}) => {
+const sendVerificationEmail = async ({ name, email, otp, type}) => {
+    if (!name || !email || !otp || !type) {
+      throw new Error('Missing required parameters: name, email, otp, or type');
+    }
 
     const message = `<p>Your OTP code is <strong>${otp}</strong>. Please use this code to verify your account.</p>`
+    const subject = `Your OTP Code for ${type} Verification`
 
     try {
         const info = await sendEmailEthereal({
             to: email,
-            subject: 'Your OTP Code for Email Verification',
+            subject,
             html: `<h4> Hello, ${name}</h4>
             ${message}
             `,
@@ -21,5 +21,6 @@ const sendVerificationEmail = async ({
         throw new Error('Error sending Otp Code')
     }
 }
+
 
 module.exports = sendVerificationEmail
